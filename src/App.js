56614d680d5dbe00d08482bc8e6a58aa0006baa9
ggrrrgg <george.sheridan@gmail.com>
2023-09-19 22:10:32 +1000

    lesson commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './App.css';
+import Pokemon from './Pokemon';
+
+// Page that lets us increase or decrease
+// how many Pokemon are on screen 
+// App component needs to:
+// - keep track of how many Pokemon to show 
+// - iterate over a single, D.R.Y modular Pokemon component 
+// - button to increase Pokemon count
+// - button to decrease Pokemon count 
+
+
+class App extends React.Component {
+  constructor(){
+    super();
+
+    // start with at least ONE pokemon on screen 
+    this.state = {
+      pokemonCount: 3
+    }
+  }
+
+  exampleFunction(){
+    console.log("Hello world, from button click");
+  }
+
+  decreasePokemonCount(currentCount){
+    if (currentCount > 0){
+      this.setState({pokemonCount: this.state.pokemonCount - 1});
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Pokemon page</h1>
+
+        {/* <button onClick={this.exampleFunction}>Increase Pokemon count</button>
+        <button onClick={() => {console.log("Hello world from inline expression")}}>Decrease Pokemon count</button> */}
+
+        <button onClick={() => {this.setState({pokemonCount: this.state.pokemonCount + 1})}}>Increase Pokemon count</button>
+        <button onClick={() => {this.decreasePokemonCount(this.state.pokemonCount)}}>
+          Decrease Pokemon count
+        </button>
+
+        {/* <Pokemon></Pokemon> */}
+        {
+          // Array(arraySize).fill(default value).map(() => {return some JSX here})
+          Array(this.state.pokemonCount).fill(null).map(() => <Pokemon />)
+        }
+
+      </div>
+    )
+  }
+
+}
+
+export default App;
---
+++ src/Pokemon.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+export default class Pokemon extends React.Component {
+	constructor(){
+		super();
+
+		this.state = {
+			pokemonData: {}
+		}
+	}
+
+    async componentDidMount() {
+        let randomPokemonNumber = Math.floor(Math.random() * 1010) + 1;
+		let apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonNumber}`);
+        let apiData = await apiResponse.json();
+
+		this.setState({pokemonData: apiData});
+    }
+
+    render(){
+		if (this.state.pokemonData.name){
+			return(
+				<div>
+					<h1>{this.state.pokemonData.name}</h1>
+					<img src={this.state.pokemonData.sprites.front_default} alt='Pokemon hey' />
+				</div>
+			)
+		} else {
+			return(
+				<div>
+					<h1>Still loading Pokemon data...</h1>
+				</div>
+			)
+		}
+	}
+}
